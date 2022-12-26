7b3f6c66f1d9d893bf017cd9ebcd74d50839a1fb
Jens Reidel <adrian@travitia.xyz>
2022-12-26 18:29:10 +0100

    build: Rename LINEAGE_BUILD to KENVYRA_BUILD, adapt check
    Change-Id: I427b5fa5e1fa28cf3d7beb902190801b7f533848
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- core/config.mk
+++ core/config.mk
@@ -357,7 +357,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(LINEAGE_BUILD),)
+ifneq ($(KENVYRA_BUILD),)
 include vendor/kenvyra/config/BoardConfigKenvyra.mk
 endif
 
@@ -1264,7 +1264,7 @@ dont_bother_goals := out product-graph
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(LINEAGE_BUILD),)
+ifneq ($(KENVYRA_BUILD),)
 ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
