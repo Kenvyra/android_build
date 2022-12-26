cf0449a24df819b99e5ae6a09880e5278b761719
Jens Reidel <adrian@travitia.xyz>
2022-12-26 18:26:27 +0100

    build: Rename BoardConfigLineage to BoardConfigKenvyra
    Change-Id: I9fa7a059ba880dd4c10a0ee3e3404facdf871212
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- core/config.mk
+++ core/config.mk
@@ -358,7 +358,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(LINEAGE_BUILD),)
-include vendor/kenvyra/config/BoardConfigLineage.mk
+include vendor/kenvyra/config/BoardConfigKenvyra.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
